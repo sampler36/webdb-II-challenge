3b793c553caa52fe0a1c8e46c9213c3f10986495
Talent Antonio <antonio.talenty@gmail.com>
2019-04-02 15:51:34 +0100

    stretch done and working

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const zooRouter = require('../zoos/zoo-router');
+const bearRouter = require('../bears/bear-router');
 
 const server = express();
 
@@ -9,5 +10,6 @@ server.use(helmet());
 server.use(express.json());
 
 server.use('/api/zoos', zooRouter);
+server.use('/api/bears', bearRouter);
 
 module.exports = server;
